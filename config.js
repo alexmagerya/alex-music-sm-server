1ac56f000519f4b17faeecf76e963338ce85259b
alexmagerya <alexmagerya@gmail.com>
2022-07-28 04:37:08 +0100

    "commit summary"

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://alexmagerya:e4X2PEtfNZP0rOO0@cluster0.ys9r84k.mongodb.net/music-sm?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+};
+//password e4X2PEtfNZP0rOO0
